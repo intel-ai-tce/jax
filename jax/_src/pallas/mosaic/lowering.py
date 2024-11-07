9dfbea0a401543611ca2c5fbc080ed3f39d64a97
Ayaka <ayx@google.com>
2024-11-06 16:16:03 -0800

    [Pallas TPU] Better error message for lowering `sp.broadcast_to_p`
    `sp.broadcast_to_p` is a GPU-specific primitive, but it mistakenly appears in TPU lowerings. This PR improves the error message to reflect this.
    
    As an example, currently, users will hit this error when doing:
    
    ```
    def kernel(x_ref, o_ref):
        m, n = 32, 8
        x = pl.load(x_ref, (jnp.arange(m, dtype=jnp.int32)[:, None], jnp.arange(n, dtype=jnp.int32)[None]))
        o_ref[...] = x
    ```
    
    PiperOrigin-RevId: 693900014

--- jax/_src/pallas/mosaic/lowering.py
+++ jax/_src/pallas/mosaic/lowering.py
@@ -1523,6 +1523,18 @@ def _reduce_or_lowering_rule(ctx: LoweringRuleContext, x, *, axes):
 lowering_rules[lax.reduce_or_p] = _reduce_or_lowering_rule
 
 
+def _broadcast_to_lowering_rule(
+    ctx: LoweringRuleContext, x, shape: Sequence[int]
+):
+  raise RuntimeError(
+      "broadcast_to is a GPU-specific primitive. Please file a bug at"
+      " https://github.com/jax-ml/jax/issues."
+  )
+
+
+lowering_rules[state_primitives.broadcast_to_p] = _broadcast_to_lowering_rule
+
+
 def _broadcast_in_dim_lowering_rule(
     ctx: LoweringRuleContext, val, *, shape, broadcast_dimensions, sharding
 ):
